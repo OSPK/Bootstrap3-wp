fe59a5864477b082f6273cbb3b8597be67d8c463
OSPK <waqas@opensource.com.pk>
2013-09-18 20:54:59 +0500

    Initial commit

---
+++ searchform.php
@@ -0,0 +1,9 @@
+<form role="search" method="get" class="col-lg-12 search-form" action="<?php echo home_url( '/' ); ?>">
+    <div class="input-group">
+    	<input type="search" class="search-field form-control" placeholder="Search …" value="" name="s">
+    	<span class="input-group-btn">
+        <button class="btn btn-default search-submit" type="button">Search</button>
+    	</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-12 -->
+</form><!-- /.form -->
